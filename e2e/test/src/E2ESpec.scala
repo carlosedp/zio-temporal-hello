b6460c51c51ac6a9cad977a7459a6928778a0406
Carlos de Paula <me@carlosedp.com>
2024-10-11 17:52:28 -0300

    Test E2E in the CI

--- e2e/test/src/E2ESpec.scala
+++ e2e/test/src/E2ESpec.scala
@@ -16,19 +16,17 @@ object E2ESpec extends ZIOSpecDefault:
     val isPortOpen = SharedUtils.temporalServer.split(":") match
         case Array(host, port) => E2ETestUtils.isPortOpen(host, port.toInt)
         case _                 => false
-    val ignoreTest =
-        if isPortOpen then TestAspect.identity else TestAspect.ignore // TODO: Check if the temporal server is running
+    val ignoreTest = if isPortOpen then TestAspect.identity else TestAspect.ignore
 
     def spec = suite("E2E"):
         test("Start worker and send msg via client"):
-            val sampleOut = """\[[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6}Z\] ACK: testmsg123""".r
             val prog =
                 for
                     _              <- Worker.worker
                     _              <- ZWorkflowServiceStubs.setup()
                     _              <- ZWorkerFactory.setup
                     workflowResult <- Client.invokeWorkflow("testmsg123")
-                yield assertTrue(sampleOut.matches(workflowResult))
+                yield assertTrue(workflowResult.contains("ACK: testmsg123"))
 
             prog.provideSome[Scope](
                 ZWorkflowClientOptions.make,
